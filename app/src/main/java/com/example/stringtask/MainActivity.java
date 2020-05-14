564022a4192704390b4b83c9c7eac916e65a6983
NaliniPuchakayala <nalini@finigoo.com>
2020-05-14 21:58:03 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stringtask/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.stringtask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Stack;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText stringEditText;
+    private Integer mStringValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        stringEditText = findViewById(R.id.inputEdittext);
+        Button addButton = findViewById(R.id.add_button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mStringValue = Integer.parseInt(stringEditText.getText().toString());
+                ;
+                str_store_fn(mStringValue);
+            }
+        });
+    }
+
+    private void str_store_fn(Integer m_enteredStr) {
+        if (m_enteredStr != null) {
+            Stack<Integer> stack_store = new Stack<Integer>();
+            stack_store.push(m_enteredStr);
+            System.out.println("Stack val: " + stack_store);
+            Dup_chk(stack_store);
+        }
+    }
+
+    private static Stack<Integer> Dup_chk(Stack<Integer> stack_store) {
+        Stack<Integer> dup_val_return = new Stack<Integer>();
+
+        int head = stack_store.peek();
+
+        while (!stack_store.isEmpty()) {
+            if (stack_store.pop() == head) {
+                int bin = stack_store.pop();
+            } else {
+                dup_val_return.push(stack_store.pop());
+            }
+        }
+        return dup_val_return;
+    }
+}
